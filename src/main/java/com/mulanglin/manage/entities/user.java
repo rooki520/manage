0d791058488569262791a59bcf1b9b72456bcd17
青梅绿蚁 <45816491+rooki@users.noreply.github.com>
2020-07-28 18:12:45 +0800

    测试

---
+++ src/main/java/com/mulanglin/manage/entities/user.java
@@ -0,0 +1,16 @@
+package com.mulanglin.manage.entities;
+
+import java.util.Date;
+
+public class user {
+    private Integer id;
+    private String mobile;
+    private String password;
+    private String sex;
+    private Date birthday;
+    private String address;
+    private String email;
+    private String state;
+    private Date create_time;
+    private Date update_time;
+}
