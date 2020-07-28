10ec578e3ed92c2b186c2b996d5eef845b7d65a2
青梅绿蚁 <45816491+rooki@users.noreply.github.com>
2020-07-28 18:20:25 +0800

    实体类

--- src/main/java/com/mulanglin/manage/entities/user.java
+++ src/main/java/com/mulanglin/manage/entities/user.java
@@ -13,4 +13,114 @@ public class user {
     private String state;
     private Date create_time;
     private Date update_time;
+
+
+    public user(Integer id, String mobile, String password, String sex, Date birthday, String address, String email, String state, Date create_time, Date update_time) {
+        this.id = id;
+        this.mobile = mobile;
+        this.password = password;
+        this.sex = sex;
+        this.birthday = birthday;
+        this.address = address;
+        this.email = email;
+        this.state = state;
+        this.create_time = create_time;
+        this.update_time = update_time;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Date getCreate_time() {
+        return create_time;
+    }
+
+    public void setCreate_time(Date create_time) {
+        this.create_time = create_time;
+    }
+
+    public Date getUpdate_time() {
+        return update_time;
+    }
+
+    public void setUpdate_time(Date update_time) {
+        this.update_time = update_time;
+    }
+
+    @Override
+    public String toString() {
+        return "user{" +
+                "id=" + id +
+                ", mobile='" + mobile + '\'' +
+                ", password='" + password + '\'' +
+                ", sex='" + sex + '\'' +
+                ", birthday=" + birthday +
+                ", address='" + address + '\'' +
+                ", email='" + email + '\'' +
+                ", state='" + state + '\'' +
+                ", create_time=" + create_time +
+                ", update_time=" + update_time +
+                '}';
+    }
 }
